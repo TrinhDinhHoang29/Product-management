dcf8fb7472ab622fbce549a003850cf56af1d8ea
TrinhDinhHoang29 <behoangloduoc@gmail.com>
2024-03-27 09:58:54 +0700

    Cấu hình xong models

--- controller/client/products.controller.js
+++ controller/client/products.controller.js
@@ -1,4 +1,8 @@
+const Product = require("../../models/products.model");
+
 
 module.exports.index=(req,res)=>{
     res.render("client/pages/products/index",{});
+    const products = Product.find({});
+    console.log(products);
 }
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express= require('express');
 const app = express();
 const routes= require("./routes/client/index.route");
 const port = process.env.PORT;
+const database = require("./config/database");
 
+database.connect();
 app.set("views","./views");
 app.set("view engine","pug");
 app.use(express.static("public"));
