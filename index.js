029390cb6da785f4a4e100abe426fa5ccb566d1b
TrinhDinhHoang29 <behoangloduoc@gmail.com>
2024-03-27 07:39:47 +0700

    Cấu hình xong env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express= require('express');
 const app = express();
 const routes= require("./routes/client/index.route");
-const port = 8080;
+const port = process.env.PORT;
 
 app.set("views","./views");
 app.set("view engine","pug");
